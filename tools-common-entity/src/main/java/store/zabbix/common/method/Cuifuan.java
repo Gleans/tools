b0304f9161cb9f69faf1632505239b0e54bfe03f
cuifuan <cuifuan@aliyun.com>
2019-03-22 11:08:06 +0800

    更新项目Eureka-Client，删除之前冗余

---
+++ tools-common-entity/src/main/java/store/zabbix/common/method/Cuifuan.java
@@ -0,0 +1,8 @@
+package store.zabbix.common.method;
+
+public class Cuifuan {
+
+    public static String test(){
+        return "cuifuan";
+    }
+}
--- tools-eureka-client/src/main/java/store/zabbix/toolseurekaclient/ToolsEurekaClientApplication.java
+++ tools-eureka-client/src/main/java/store/zabbix/toolseurekaclient/ToolsEurekaClientApplication.java
@@ -4,15 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+
 @RestController
 @EnableEurekaClient
-@SpringBootApplication
-@MapperScan("store.zabbix.*.mapper")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ToolsEurekaClientApplication {
 
     public static void main(String[] args) {
